ba3335bd15cffb87984455614575f6f3c8e660e7
Charlotte <charlotte.basister@gmail.com>
2021-07-05 14:24:13 +0800

    updated shelves ajax - get all

--- public/js/ajax/librarian/shelves.ajax.js
+++ public/js/ajax/librarian/shelves.ajax.js
@@ -8,8 +8,99 @@
 
 
 $(() => {
+    loadShelvesDT();
     shelves_countAJAX();
 })
+
+/**
+ * ===============================================================================
+ * GET ALL SHELVES AJAX
+ * ===============================================================================
+ */
+
+// Load shelves DataTable
+loadShelvesDT = () => {
+    const dt = $('#shelvesDT');
+    if(dt.length){
+        dt.DataTable({
+            ajax: {
+                url: `${ BASE_URL_API }librarian/shelves`,
+                type: 'GET',
+                headers: AJAX_HEADERS,
+                // success: (result) => {
+                //     if(result){
+                //         const data = result.data;
+                //         console.log(data);
+                //     }
+                //     else{
+                //         console.log('No result');
+                //     }
+                // }
+            },
+            columns: [
+                {
+                    data: 'shelfName'
+                },
+                
+                {
+                    data: null,
+                    render: (data) =>{
+                        if(data.status == 'Active'){
+                            return `
+                                <div class="badge alert-success text-success p-2 w-100">Active</div>
+                            `
+                        }
+                        else{
+                            return `
+                                <div class="badge alert-danger text-danger p-2 w-100">Inactive</div>
+                            `
+                        }
+                    }
+                },
+                {
+                    data: null,
+                    class: 'text-center',
+                    render: (data) => {
+                        return `
+                            <div class="dropdown d-inline-block">
+                                <div data-toggle="dropdown">
+                                    <div 
+                                        class       = "btn btn-sm btn-muted"
+                                        data-toggle = "tooltip"
+                                        title       = "More"
+                                    >
+                                        <i class="fas fa-ellipsis-v"></i>
+                                    </div>
+                                </div>
+
+                                <div class="dropdown-menu dropdown-menu-right">
+                                    <div 
+                                        class="dropdown-item"
+                                        onclick = "editAuthor('${data.shelfID}')"
+                                    >
+                                        <i class="fas fa-edit dropdown-icon-item text-blue"></i>
+                                        <span>Edit</span>
+                                    </div>
+                                    <div 
+                                        class="dropdown-item"
+                                        onclick = "removeAuthor('${data.shelfID}')"
+                                    >
+                                        <i class="fas fa-trash-alt dropdown-icon-item text-danger"></i>
+                                        <span>Remove</span>
+                                    </div>
+                                </div>
+                            </div>
+                        `
+                    }
+                },
+            ],
+            columnDefs: [{
+                targets: [2],
+                orderable: false,
+            }]
+        })
+    }
+}
 
 
 /**
