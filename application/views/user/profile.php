f61ab44867be5557c7d7157cfe90cc4a8327e941
Vanessah Buenaventura <68227233+Bandroite@users.noreply.github.com>
2021-07-23 14:33:41 +0800

    Borrower Info

--- application/views/user/profile.php
+++ application/views/user/profile.php
@@ -1,12 +1,46 @@
 <div class="container py-5">
-    <div class="row">
-        <div class="col-md-3">
+    <!-- Header -->
+    <div class="mb-4">
+        <h3>This is your personal information</h3>
+        <h6 class="text-secondary">View your information here</h6>
+    </div>
+
+    <div class="row" id="borrowerProfile">
+        <div class="col-lg-4 mb-4 mb-sm-0">
             <div class="card">
-                <div class="card-body">
-                    <img src="<?= base_url() ?>" alt="">
+                <div class="card-body p-3">
+                    <div class="flex-center mb-4">
+                        <img 
+                            class="rounded-circle" 
+                            id="avatar"
+                            style="height: 15rem; width: 15rem"
+                        >
+                    </div>
+                    <div class="text-center">
+                        <h4 class="mb-1" id="borrowerFullNameForProfile"></h4>
+                        <h6 class="text-secondary mb-1" id="borrowerIDNumberForProfile"></h6>
+                        <div class="badge alert-primary text-primary px-2" id="borrowerTypeForProfile"></div>
+                        <div class="mt-3" id="borrowerEmailForProfile"></div>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col-md-9"></div>
+        <div class="col-lg-8">
+            <div class="card">
+                <div class="card-body">
+                    <h4>
+                        <i class="fas fa-info-circle mr-1 text-primary"></i>
+                        <span>Personal Information</span>
+                    </h4>
+                    <table class="table mt-3" id="borrowerDetails"></table>
+                    <div class="form-group text-center">
+                        <button class="btn btn-blue">
+                            <i class="fas fa-edit mr-1"></i>
+                            <span>Edit your information</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
--- public/js/ajax/borrower/account.ajax.js
+++ public/js/ajax/borrower/account.ajax.js
@@ -25,9 +25,106 @@ get_borrower_infoAJAX = () => {
             if(result){
                 const data = result.data;
                 
-                console.log(data);
+                const borrowerFullName = setFullName('F M L', {
+                    firstName: data.firstName,
+                    middleName: data.middleName,
+                    lastName: data.lastName
+                });
+
 
                 $('#borrowerFirstName').html(data.firstName);
+
+                // For Profile
+                if(('borrowerProfile').length) {
+                    const userType = data.userType;
+                    
+                    $('#borrowerFullNameForProfile').html(borrowerFullName);
+                    $('#borrowerIDNumberForProfile').html(data.idNumber);
+                    $('#borrowerTypeForProfile').html(userType);
+                    $('#borrowerEmailForProfile').html(data.email);
+
+                    var sexIcon;
+                    if(data.gender === 'Male') {
+                        $('#avatar').attr('src', `${ BASE_URL_WEB }public/images/app/avatar_male.png`);
+                        sexIcon =`<i class="fas fa-mars text-blue mr-1"></i>`
+                    } else if(data.gender === 'Female') {
+                        $('#avatar').attr('src', `${ BASE_URL_WEB }public/images/app/avatar_female.png`);
+                        sexIcon =`<i class="fas fa-venus text-danger mr-1"></i>`
+                    } else {
+                        $('#avatar').attr('src', `${ BASE_URL_WEB }public/images/app/avatar_genderless.png`);
+                        sexIcon =`<i class="fas fa-genderless text-secondary mr-1"></i>`
+                    }
+
+                    if(userType === 'Student') {
+                        $('#borrowerDetails').html(`
+                            <tr>
+                                <th>Full Name</th>
+                                <td>${ borrowerFullName }</td>
+                            </tr>
+                            <tr>
+                                <th>Student Number</th>
+                                <td>${ data.idNumber }</td>
+                            </tr>
+                            <tr>
+                                <th>Course, Year and Section</th>
+                                <td>${ data.course }, ${ data.year } - ${ data.section }</td>
+                            </tr>
+                            <tr>
+                                <th>Gender</th>
+                                <td>
+                                    ${ sexIcon }
+                                    <span>${ data.gender }</span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th>Email</th>
+                                <td>
+                                    <i class="fas fa-at text-secondary mr-1"></i>
+                                    <span>${ data.email }</span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th>Contact Number</th>
+                                <td>
+                                    <i class="fas fa-phone text-secondary mr-1"></i>
+                                    <span>${ data.contactNumber }</span>
+                                </td>
+                            </tr>
+                        `)
+                    } else if(userType === 'Staff') {
+                        $('#borrowerDetails').html(`
+                            <tr>
+                                <th>Full Name</th>
+                                <td>${ borrowerFullName }</td>
+                            </tr>
+                            <tr>
+                                <th>Student Number</th>
+                                <td>${ data.idNumber }</td>
+                            </tr>
+                            <tr>
+                                <th>Gender</th>
+                                <td> 
+                                    ${ sexIcon }
+                                    <span>${ data.gender }</span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th>Email</th>
+                                <td>
+                                    <i class="fas fa-at text-secondary mr-1"></i>
+                                    <span>${ data.email }</span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th>Contact Number</th>
+                                <td>
+                                    <i class="fas fa-phone text-secondary mr-1"></i>
+                                    <span>${ data.contactNumber }</span>
+                                </td>
+                            </tr>
+                        `);
+                    }
+                }
             }
             else{
                 console.log('No result');
